d6ca10653bdf64669437f09793f0fc91046dee19
ChloeSAPage <chloe.s.a.page@Gmail.com>
2023-09-08 19:57:18 +0100

    initial commit

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+import os
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+client.run(TOKEN)
